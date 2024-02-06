2b662b81bf3735c37463f68e9ddcee32dece8081
rid999 <110356281+rid999@users.noreply.github.com>
2024-02-06 12:16:59 +0700

    Update cekapi2.sh

--- cekapi2.sh
+++ cekapi2.sh
@@ -1,8 +1,3 @@
-# Function to print a red cross
-print_cross() {
-  echo -e "[\e[31m✘\e[0m] Not Enabled"
-}
-
 # Function to enable API
 enable_api() {
   local api=$1
@@ -12,16 +7,16 @@ enable_api() {
 
 # List of APIs to check and enable
 apis=(
-  "1. admin.googleapis.com"
-  "2. drive.googleapis.com"
-  "3. gmail.googleapis.com"
-  "4. calendar-json.googleapis.com"
-  "5. people.googleapis.com"
-  "6. tasks.googleapis.com"
-  "7. forms.googleapis.com"
-  "8. groupsmigration.googleapis.com"
-  "9. vault.googleapis.com"
-  "10. storage-component.googleapis.com"
+  "admin.googleapis.com"
+  "drive.googleapis.com"
+  "gmail.googleapis.com"
+  "calendar-json.googleapis.com"
+  "people.googleapis.com"
+  "tasks.googleapis.com"
+  "forms.googleapis.com"
+  "groupsmigration.googleapis.com"
+  "vault.googleapis.com"
+  "storage-component.googleapis.com"
 )
 
 echo "Checking if APIs have been enabled:"
@@ -32,19 +27,18 @@ echo "------------------------------------------------------------------"
 # Check and enable APIs that are not verified
 for ((i=0; i<${#apis[@]}; i++)); do
   api="${apis[$i]}"
-  api_number=$(echo "$api" | cut -d' ' -f1)
-  api_name=$(echo "$api" | cut -d' ' -f2-)
-  status=$(gcloud services list --format="value(NAME)" --filter="NAME:$api_name")
+  api_number=$((i+1))
+  status=$(gcloud services list --format="value(NAME)" --filter="NAME:$api")
 
-  if [[ "$status" != "$api_name" ]]; then
-    enable_api "$api_name"
-    status=$(gcloud services list --format="value(NAME)" --filter="NAME:$api_name")
+  if [[ "$status" != "$api" ]]; then
+    enable_api "$api"
+    status=$(gcloud services list --format="value(NAME)" --filter="NAME:$api")
   fi
 
-  status_message=$(print_cross)
-  [[ "$status" == "$api_name" ]] && status_message="Enabled"
+  status_message="Not Enabled"
+  [[ "$status" == "$api" ]] && status_message="Enabled"
 
-  printf "| %-5s | %-38s | %-11s |\n" "$api_number" "$api_name" "$status_message"
+  printf "| %-5s | %-38s | %-11s |\n" "$api_number" "$api" "$status_message"
 done
 
 echo "------------------------------------------------------------------"
